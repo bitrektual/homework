07bcb00320094ff48c4ce282722f5ddd951357ab
bitrektual <74793416+bitrektual@users.noreply.github.com>
2020-12-25 18:43:41 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,9 +7,13 @@ class TestQueue(unittest.TestCase):
 
     def test_enqueue_dequeue(self):
         queue_for_tests = Queue()
-        value = 1
-        queue_for_tests.enqueue(value)
-        self.assertEqual(queue_for_tests.tail_of_queue(), value)
+        first_value = 1
+        second_value = 2
+        third_value = 3
+        queue_for_tests.enqueue(first_value)
+        queue_for_tests.enqueue(second_value)
+        queue_for_tests.enqueue(third_value)
+        self.assertEqual(queue_for_tests.tail_of_queue(), third_value)
         self.assertEqual(queue_for_tests.head_of_queue(), queue_for_tests.dequeue())
         
     def test_empty(self):
