42a2bd5fbef6bfa5476b35408426a8e8d2db2e7b
bitrektual <74793416+bitrektual@users.noreply.github.com>
2020-12-25 17:34:40 +0300

    Update myqueue.py

--- myqueue.py
+++ myqueue.py
@@ -8,16 +8,19 @@ class Queue:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.size = None
 
     def enqueue(self, newvalue):
         newelement = Element(newvalue)
         if self.head is None:
             self.head = newelement
             self.tail = self.head
+            self.size = 1
             return
         lastelement = self.tail
         self.tail = newelement
         lastelement.prev = newelement
+        self.size = self.size + 1
 
     def dequeue(self):
         if self.head is None:
@@ -27,12 +30,13 @@ class Queue:
         return takenelement.value
 
     def size(self):
-        lastinque = self.head
-        sizeofqueue = 0
-        while (lastinque):
-            sizeofqueue = sizeofqueue + 1
-            lastinque = lastinque.prev
-        return sizeofqueue
+        return self.size
+
+    def head(self):
+        return self.head
+
+    def tail(self):
+        return self.tail
 
     def clear(self):
         while (self.head):
